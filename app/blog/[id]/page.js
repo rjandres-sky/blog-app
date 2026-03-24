3d289dbe526de83b589cf928720fbc3ccf1e4d85
Rj Andres <102931013+rjandres-sky@users.noreply.github.com>
2026-03-24 15:24:57 +0800

    added styling and Back Link in sigle blog page

--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -1,15 +1,16 @@
-
-export default async function SingleBlog({ params }) {
+import Link from 'next/link';
+export default async function BlogPost({ params }) {
   const { id } = await params;
-
-  const fetchBlogPost = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-  const blogPost = await fetchBlogPost.json();
-
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const post = await res.json();
 
   return (
-    <div key={blogPost.id} className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">{blogPost.title}</h1>
-      <p className="mt-4 text-lg">{blogPost.body}</p>
+    <div className="p-8 max-w-2xl mx-auto">
+        <Link href="/blog" className="text-2xl font-bold text-blue-600 mb-4">
+                ← Back
+        </Link>
+      <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
+      <p className="text-lg text-gray-700 leading-relaxed">{post.body}</p>
     </div>
   );
 }
